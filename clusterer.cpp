5ffb27815fea55e993aeaa2c0777753daed4402a
Bonolo <mlpbon002@myuct.ac.za>
2020-04-04 13:03:59 +0200

    Changed method definition to read all images and put read data into 2d array

--- clusterer.cpp
+++ clusterer.cpp
@@ -10,51 +10,60 @@
 #include "clusterer.h"
 using namespace MKHSIN035;
 using namespace std;
+
+/*
+ * Default Constructor definition
+ */
 MKHSIN035::KMeansClusterer::KMeansClusterer(){}
-void KMeansClusterer::readDataSet(){
-    string filename = "Gradient_Numbers_PPMS/eight_1.ppm";
-    //Open the file in binary mode
-    //Position the pointer at the end of the file
-    ifstream dataset(filename, ios::binary|ios::ate); 
-    streampos begin,end;
+/*
+ * Destructor definition
+ */
+MKHSIN035::KMeansClusterer::~KMeansClusterer(){}
 
-    if(dataset.is_open()){
-        end = dataset.tellg();
-        int Nrows, Ncols;
-        string dim;
-        string line;
-                
-        //Take pointer back to the beginning of the file
-        dataset.seekg(0, ios::beg);
-        
-        while(getline(dataset, line)){
-            if(line == "255"){
-                
-                begin = dataset.tellg();
+void KMeansClusterer::readDataSet(string folder){
+    string extension = ".ppm";
+    string filename;
+    ifstream dataset;
+    images = new unsigned char* [100];
+    int counter = 0;
+    for (int i = 0; i < 10; i++){
+        for(int j = 1; j <=10; j++ ){
+            filename = folder+filenames[i]+to_string(j)+extension;
             
-                
+            dataset.open(filename, ios::binary|ios::ate); 
+            streampos begin,end;
+            
+            if(dataset.is_open()){
+                end = dataset.tellg();
+                string dim;
+                string line;
+
+                //Take pointer back to the beginning of the file
+                dataset.seekg(0, ios::beg);
+
+                while(getline(dataset, line)){
+                    if(line == "255"){
+                        begin = dataset.tellg();
+                    }
+                }
+                images[counter] = new unsigned char[end-begin];
+                char buf[end-begin];
+                unsigned char val [end-begin];
+                dataset.read(buf, (end-begin));
+                memcpy(&val, buf, sizeof(val));
+                images[counter] = val; 
+                            
+                cout<<"Done reading: "<<filename<<endl;
+
             }
-
+            else{
+                cout<<"Could not open file "<<filename<<endl;
+            }
+            dataset.close();
+            counter++;
         }
-        KMeansClusterer::image = new unsigned char[end - begin];
-        char buf[end-begin];
-        unsigned char val [end-begin];
-        dataset.read(buf, (end-begin));
-        memcpy(&val, buf, sizeof(val));
-        image = val;    
-        for(int i = 0;  i < end-begin; i++){
-            cout<<+image[i]<<" ";
-
-        }
-        
-        cout<<" Done"<<endl;
-        
-        dataset.close();
-
     }
-    else{
-        cout<<"Could not open file "<<filename<<endl;
-    }
+    
 }
 
 ostream& operator<<(std::ostream& os, const KMeansClusterer& kt){
