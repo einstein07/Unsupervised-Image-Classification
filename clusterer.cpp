ab266c1a53ee9e2e48732e1cac83ecb1c938597e
Bonolo <mlpbon002@myuct.ac.za>
2020-04-05 13:46:05 +0200

    Method definitions of class Image

--- clusterer.cpp
+++ clusterer.cpp
@@ -12,24 +12,59 @@ using namespace MKHSIN035;
 using namespace std;
 
 /*
- * Default Constructor definition
+ * Default Constructor definition for Image class
+ */
+MKHSIN035::Image::Image(std::string name):filename(name){}
+
+/*
+ * Destructor definition for KMeansClusterer
+ */
+MKHSIN035::Image::~Image(){}
+/*
+ * Default Constructor definition for KMeansClusterer
  */
 MKHSIN035::KMeansClusterer::KMeansClusterer(){}
+
 /*
- * Destructor definition
+ * Destructor definition for KMeansClusterer
  */
 MKHSIN035::KMeansClusterer::~KMeansClusterer(){}
+
+/*
+ * setintensitylen definition
+ */
+void MKHSIN035::Image::setintensitylen(int len){intensitylen = len;}
+
+/*
+ * setfeaturelen definition
+ */
+void MKHSIN035::Image::setfeaturelen(int len){featurelen = len;}
+
+/*
+ * getintensitylen definition
+ */
+int MKHSIN035::Image::getintensitylen(){return intensitylen;}
+
+/*
+ * getintensitylen definition
+ */
+int MKHSIN035::Image::getfeaturelen(){return featurelen;}
+
+/*
+ * getfilename definition
+ */
+string MKHSIN035::Image::getfilename(){return filename;}
 
 void KMeansClusterer::readDataSet(string folder){
     string extension = ".ppm";
     string filename;
     ifstream dataset;
-    images = new unsigned char* [100];
+    //images = new unsigned char* [100];
     int counter = 0;
     for (int i = 0; i < 10; i++){
         for(int j = 1; j <=10; j++ ){
             filename = folder+filenames[i]+to_string(j)+extension;
-            
+            Image obj(filename);
             dataset.open(filename, ios::binary|ios::ate); 
             streampos begin,end;
             
@@ -46,20 +81,17 @@ void KMeansClusterer::readDataSet(string folder){
                         begin = dataset.tellg();
                     }
                 }
-                images[counter] = new unsigned char[(end-begin)+1];
-                char buf[end-begin];
-                unsigned char val [end-begin];
-                dataset.read(buf, (end-begin));
+                obj.setintensitylen(end-begin);
+                obj.intensity = new unsigned char[(obj.getintensitylen())];
+                char buf[obj.getintensitylen()];
+                unsigned char val [obj.getintensitylen()];
+                dataset.read(buf, obj.getintensitylen());
                 memcpy(&val, buf, sizeof(val));
-                //Store the size first
-                images[counter][0] = (end-begin);
-                for (int k = 1; k <= ((end-begin)+1); k++){
-                    images[counter][k] = val[k-1];
-                    //cout<<+images[counter][k]<<" ";
-                }
-                 
-                cout<<endl;            
-                cout<<"Image "<<i<<" length: "<< (end-begin)<<endl;
+                obj.intensity = val;
+                //Add image to collection of images
+                images.push_back(obj);
+                
+                
 
             }
             else{
@@ -70,38 +102,49 @@ void KMeansClusterer::readDataSet(string folder){
         }
     }
     
+    for(int i = 0; i < images.size(); i++){
+                                        
+        cout<<"\nDone reading image "<<images[i].getfilename()<<endl;            
+
+        for (int j = 0; j < images[i].getintensitylen(); j++){
+            cout<<+images[i].intensity[j]<<" ";            
+
+        }
+
+    }
+    
 }
 
 void MKHSIN035::KMeansClusterer::imageFeature(int bin){
-    //feature = new int* [100];
-    
-    int Lbound = 0;
-    int Ubound = bin;
-    
-    
-    for(int i = 0; i < 100; i++){
-        cout<<"Size of image "<<(i+1)<<": "<<+images[i][0]<<endl;
-        int entries = images[i][0]/bin;
-        vector <int> features(entries,0);
-
-        for (int k = 0; k < entries; k++){
-            for (int j = 0; j <= images[i][0]; j++){
-                if (images[i][j] >= Lbound && images[i][j] < Ubound){
-                    features[k]++;
-                }
-
-            }
-                  
-            Lbound += bin;
-            Ubound += bin;
-
-        }
-                    
-        cout<<"Done creating feature for image: "<<i<<". Num of entries: "<<entries<<endl;
-
-        feature.push_back(features);
+//    //feature = new int* [100];
+//    
+//    int Lbound = 0;
+//    int Ubound = bin;
+//    
+//    
+//    for(int i = 0; i < 100; i++){
+////        cout<<"Size of image "<<(i+1)<<": "<<+images[i][0]<<endl;
+////        int entries = images[i][0]/bin;
+////        vector <int> features(entries,0);
+//
+//        for (int k = 0; k < entries; k++){
+//            for (int j = 0; j <= images[i][0]; j++){
+//                if (images[i][j] >= Lbound && images[i][j] < Ubound){
+//                    features[k]++;
+//                }
+//
+//            }
+//                  
+//            Lbound += bin;
+//            Ubound += bin;
+//
+//        }
+//                    
+//        cout<<"Done creating feature for image: "<<i<<". Num of entries: "<<entries<<endl;
+//
+//        feature.push_back(features);
         
-    }
+//    }
 }
 
 ostream& operator<<(std::ostream& os, const KMeansClusterer& kt){
