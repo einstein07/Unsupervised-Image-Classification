21f8bf318db0589280963a5932cac6fa82f56c3e
Bonolo <mlpbon002@myuct.ac.za>
2020-04-05 18:54:46 +0200

    Added method definition for imageFeature(int)

--- clusterer.cpp
+++ clusterer.cpp
@@ -33,7 +33,7 @@ MKHSIN035::KMeansClusterer::~KMeansClusterer(){}
 /*
  * setintensitylen definition
  */
-void MKHSIN035::Image::setintensitylen(int len){intensitylen = len;}
+void MKHSIN035::Image::setgreyscalelen(int len){greyscalelen = len;}
 
 /*
  * setfeaturelen definition
@@ -43,7 +43,7 @@ void MKHSIN035::Image::setfeaturelen(int len){featurelen = len;}
 /*
  * getintensitylen definition
  */
-int MKHSIN035::Image::getintensitylen(){return intensitylen;}
+int MKHSIN035::Image::getgreyscalelen(){return greyscalelen;}
 
 /*
  * getintensitylen definition
@@ -81,13 +81,20 @@ void KMeansClusterer::readDataSet(string folder){
                         begin = dataset.tellg();
                     }
                 }
-                obj.setintensitylen(end-begin);
-                obj.intensity = new unsigned char[(obj.getintensitylen())];
-                char buf[obj.getintensitylen()];
-                unsigned char val [obj.getintensitylen()];
-                dataset.read(buf, obj.getintensitylen());
+                obj.setgreyscalelen(((end-begin)/3));
+                obj.greyscale = new unsigned char[(obj.getgreyscalelen())];
+                char buf[end-begin];
+                unsigned char val [end-begin];
+                dataset.read(buf, end-begin);
                 memcpy(&val, buf, sizeof(val));
-                obj.intensity = val;
+                
+                int count = 0;
+                for (int c = 0; c < obj.getgreyscalelen(); c++){
+                    obj.greyscale[c] = 0.21*val[count] + 0.72*val[count+1] + 0.07*val[count+2];
+                    count += 3;
+                }
+                
+                //obj.intensity = val;
                 //Add image to collection of images
                 images.push_back(obj);
                 
@@ -104,10 +111,10 @@ void KMeansClusterer::readDataSet(string folder){
     
     for(int i = 0; i < images.size(); i++){
                                         
-        cout<<"\nDone reading image "<<images[i].getfilename()<<endl;            
+        cout<<"\nDone reading image "<<images[i].getfilename()<<"\nSize: "<<images[i].getgreyscalelen()<<endl;            
 
-        for (int j = 0; j < images[i].getintensitylen(); j++){
-            cout<<+images[i].intensity[j]<<" ";            
+        for (int j = 0; j < images[i].getgreyscalelen(); j++){
+            cout<<+images[i].greyscale[j]<<" ";            
 
         }
 
@@ -116,35 +123,52 @@ void KMeansClusterer::readDataSet(string folder){
 }
 
 void MKHSIN035::KMeansClusterer::imageFeature(int bin){
-//    //feature = new int* [100];
-//    
-//    int Lbound = 0;
-//    int Ubound = bin;
-//    
-//    
-//    for(int i = 0; i < 100; i++){
-////        cout<<"Size of image "<<(i+1)<<": "<<+images[i][0]<<endl;
-////        int entries = images[i][0]/bin;
-////        vector <int> features(entries,0);
-//
-//        for (int k = 0; k < entries; k++){
-//            for (int j = 0; j <= images[i][0]; j++){
-//                if (images[i][j] >= Lbound && images[i][j] < Ubound){
-//                    features[k]++;
-//                }
-//
-//            }
-//                  
-//            Lbound += bin;
-//            Ubound += bin;
-//
-//        }
-//                    
-//        cout<<"Done creating feature for image: "<<i<<". Num of entries: "<<entries<<endl;
-//
-//        feature.push_back(features);
+    int featurelen = 256/bin;
+    
+    
+    
+   
+    for(int i = 0; i < images.size(); i++){
+        images[i].feature = new int[featurelen];
+        images[i].setfeaturelen(featurelen);
+        int Lbound = 0;
+        int Ubound = bin;
+    
+        for (int a = 0; a < images[i].getfeaturelen(); a++){
+            images[i].feature[a] = 0;
+        }
+
         
-//    }
+        
+
+        for (int k = 0; k < images[i].getfeaturelen(); k++){
+            for (int j = 0; j <= images[i].getgreyscalelen(); j++){
+                if (Lbound <= images[i].greyscale[j]  && images[i].greyscale[j] < Ubound){
+                    images[i].feature[k]++;
+                }
+
+            }
+               
+            Lbound += bin;
+            Ubound += bin;
+
+        }
+                    
+        //cout<<"Done creating feature for image: "<<i<<endl;
+
+        
+    }
+    
+    for(int i = 0; i < images.size(); i++){
+                                        
+        cout<<"Image "<<images[i].getfilename()<<" Feature length: "<<images[i].getfeaturelen()<<endl;            
+
+        for (int j = 0; j < images[i].getfeaturelen(); j++){
+            cout<<images[i].feature[j]<<" ";            
+
+        }
+
+    }
 }
 
 ostream& operator<<(std::ostream& os, const KMeansClusterer& kt){
