c46b9e30799622c06e34ae279945cc6049f0a3a4
Sindiso Mkhatshwa <mkhsin035@myuct.ac.za>
2020-05-06 01:31:17 +0200

    Changed main file code to accomodate changes made in Image class and KMeansClusterer class

--- main.cpp
+++ main.cpp
@@ -22,6 +22,8 @@ using namespace std;
  * 
  */
 int main(int argc, char** argv) {
+    
+    
     //get the location of the folder that contains the images to be clustered
     string dataset = argv[1];
     string outputfile;
@@ -38,23 +40,29 @@ int main(int argc, char** argv) {
         bin = atoi(argv[4]);
     if(argc > 5)
         color = true;
-    cout<<"Main k: "<<k<<endl;
+    
+    std::string filenames [10] = {"zero_", "one_", "two_", "three_", "four_",
+            "five_", "six_", "seven_", "eight_", "nine_"};
+    
     KMeansClusterer obj(k, color, bin);
-    Image im(dataset);
-    string file = dataset+"eight_10.ppm";
-    cout<<"File name: "<<file<<endl;
-    im.read(file);
-    im.createFeature(bin, true);
-    //obj.readDataSet();
-    //obj.createFeature();
-//    obj.kmeansclustering();
-//    const KMeansClusterer oj = obj;
-//    if(outputfile == "")
-//        cout<<oj;
-//    else{
-//        ofstream file(outputfile);
-//        file<<oj;
-//    }
+    for(int i = 0; i < 10; i++){
+        for(int j = 1; j <= 10; j++){
+            string filename = filenames[i]+to_string(j);
+            Image im(dataset, color);
+            im.read(filename);
+            im.createFeature(bin);
+            obj.images.push_back(im);
+        }
+    }
+  
+    obj.kmeansclustering();
+    const KMeansClusterer oj = obj;
+    if(outputfile == "")
+        cout<<oj;
+    else{
+        ofstream file(outputfile);
+        file<<oj;
+    }
     return 0;
 }
 
